36c1caca339e0ef0bd4dd8f5532f1ef328858e86
Afonso Águas <afonsoaguas@gmail.com>
2019-09-18 22:21:24 +0100

    Toasty

--- src/android/ToastyPlugin.java
+++ src/android/ToastyPlugin.java
@@ -7,11 +7,18 @@ import android.widget.Toast;
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.PluginResult;
+import org.apache.cordova.CordovaActivity;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import android.view.autofill.AutofillManager;
 import android.content.Context;
+import android.app.Activity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+
+
 
 public class ToastyPlugin extends CordovaPlugin {
   
@@ -25,11 +32,20 @@ public class ToastyPlugin extends CordovaPlugin {
         callbackContext.error("\"" + action + "\" is not a recognized action.");
         return false;
       }
+      cordova.getActivity().runOnUiThread(new Runnable() {
+        @SuppressWarnings("ResourceType")
+        @Override
+        public void run() {
 
-      Context context = this.cordova.getActivity().getApplicationContext();
-      AutofillManager autofillManager = context.getSystemService(AutofillManager.class);
-      autofillManager.disableAutofillServices();
+            final CordovaActivity cordovaActivity = (CordovaActivity) cordova.getActivity();
 
+            ViewGroup mainViewGroup = (ViewGroup) webView.getView().getParent();
+
+            ViewGroup rootView = (ViewGroup) mainViewGroup.getParent();
+
+            rootView.setImportantForAutofill(IMPORTANT_FOR_AUTOFILL_NO_EXCLUDE_DESCENDANTS);
+        }
+      });
       return true;
   }
 }
